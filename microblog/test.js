598f6c1486e53d086be6194e077ed3d80950b51e
Your Name <yangjie5@yangjie4-38.tech.bitauto.com>
2016-07-13 18:02:17 +0800

    v1.0.6

--- microblog/test.js
+++ microblog/test.js
@@ -2,12 +2,19 @@
  * Created by yangjie5 on 2016/6/14.
  */
 var fs = require('fs');
-fs.readdir("views/article/", function (err, files) {
+var markdown = require( "markdown" ).markdown;
+
+fs.readFile("views/article/a3.md",function(err, data){
     if (err) {
         return console.error(err);
     }
-    files.forEach(function (file) {
-        console.log( file );
+    //console.log("异步读取: " + data);
+    var html = markdown.toHTML(data.toString());
+    fs.writeFile('views/article/4.ejs', html,  function(err) {
+        if (err) {
+            return console.error(err);
+        }
+        console.log("数据写入成功！");
     });
 });
-var i=0;
+
